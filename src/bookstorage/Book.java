454fd348e0ee05450631d4b52efd74ed3ad50b7c
Kayln Shaw <kaylns@mwc-032011.dhcp.missouri.edu>
2025-02-11 17:22:53 -0600

    creating def/param constructors

--- src/bookstorage/Book.java
+++ src/bookstorage/Book.java
@@ -5,4 +5,17 @@ public class Book {
  private String author;
  private String ISBN;
  private double price;
+ 
+public Book() {
+	this.title = "Unknown";
+	this.author = "Unknown";
+	this.ISBN = "Unknown";
+	this.price = 0.0;
+}
+public Book(String InTitle, String InAuthor, String InISBN, double InPrice) {
+	this.title = InTitle;
+	this.author = InAuthor;
+	this.ISBN = InISBN; 
+	this.price = InPrice;
+}
 }
