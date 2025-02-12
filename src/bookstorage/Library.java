a05827786b2f9fc74f1fb6c16d238b93c1b37ae0
Amwaj <117525912+amwaj2004@users.noreply.github.com>
2025-02-11 18:02:11 -0600

    addBook method created Library.java

--- src/bookstorage/Library.java
+++ src/bookstorage/Library.java
@@ -4,11 +4,32 @@ public class Library {
 	private Book[] books;
 	private int count;
 	
+	/** 
+	 * Default constructor; initalize Library attributes
+	 */
 	public Library() {
 		this.books = new Book[5];
 		this.count = 0;
 	}
-}
 	
+	/**
+	 * This method adds a book into the Book array if there is an open space
+	 * @param book; this is the book it's adding
+	 * @return; if it finds an open space and adds the book it will return true
+	 * 			other-wise it will return false
+	 */
+	public boolean addBook(Book book) {
+		for(int i = 0; i < books.length; i++) {
+			if(books[i] == null) {
+				books[i] = book;
+				count++;
+				return true;
+			}
+		}
+		return false;
+	}
+
+}	
+
 
  
