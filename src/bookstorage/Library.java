6346cf2bb8e8de472e74592c8e23d344121756d3
Amwaj <117525912+amwaj2004@users.noreply.github.com>
2025-02-11 18:14:56 -0600

    removeBook method added on Library.java

--- src/bookstorage/Library.java
+++ src/bookstorage/Library.java
@@ -14,6 +14,7 @@ public class Library {
 	
 	/**
 	 * This method adds a book into the Book array if there is an open space
+	 * It will increment the count if it adds a book
 	 * @param book; this is the book it's adding
 	 * @return; if it finds an open space and adds the book it will return true
 	 * 			other-wise it will return false
@@ -24,6 +25,24 @@ public class Library {
 				books[i] = book;
 				count++;
 				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * This method removes a book from the Book array if it matches the given 'book'
+	 * It will decrement the count if it removes one
+	 * @param book; this is the book we're removing
+	 * @return; if it finds the book we're trying to remove it will remove it and return true
+	 * 			other-wise it will return false
+	 */
+	public boolean removeBook(Book book) {
+		for(int i = 0; i < books.length; i++) {
+			if(books[i] == book) {
+				books[i] = null;
+				count--;
+				return true; 
 			}
 		}
 		return false;
