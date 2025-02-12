160474b203be30d8c245ede6c515c8d136d1220f
Kayln Shaw <kaylns@Kaylns-MacBook-Pro.local>
2025-02-12 16:37:14 -0600

    added getters and setters

--- src/bookstorage/Book.java
+++ src/bookstorage/Book.java
@@ -45,8 +45,31 @@ public Book(Book copy) {
 	this.ISBN = copy.ISBN;
 	this.price = copy.price;
 }
-
 //GETTERS AND SETTERS
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getAuthor() {
+	return author;
+}
+public void setAuthor(String author) {
+	this.author = author;
+}
+public String getISBN() {
+	return ISBN;
+}
+public void setISBN(String iSBN) {
+	this.ISBN = iSBN;
+}
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
 
 
 }
