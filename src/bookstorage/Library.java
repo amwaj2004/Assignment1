fce39e66e4edfdcaf605ad12e5265be03cc4ee40
Amwaj <117525912+amwaj2004@users.noreply.github.com>
2025-02-12 19:09:06 -0600

    added searchByISBN and displayBooks method in Library.java

--- src/bookstorage/Library.java
+++ src/bookstorage/Library.java
@@ -1,5 +1,7 @@
 package bookstorage;
 
+import java.util.Arrays;
+
 public class Library {
 	private Book[] books;
 	private int count;
@@ -17,7 +19,7 @@ public class Library {
 	 * It will increment the count if it adds a book
 	 * @param book; this is the book it's adding
 	 * @return; if it finds an open space and adds the book it will return true
-	 * 			other-wise it will return false
+	 * other-wise it will return false
 	 */
 	public boolean addBook(Book book) {
 		for(int i = 0; i < books.length; i++) {
@@ -35,7 +37,7 @@ public class Library {
 	 * It will decrement the count if it removes one
 	 * @param book; this is the book we're removing
 	 * @return; if it finds the book we're trying to remove it will remove it and return true
-	 * 			other-wise it will return false
+	 * other-wise it will return false
 	 */
 	public boolean removeBook(Book book) {
 		for(int i = 0; i < books.length; i++) {
@@ -47,7 +49,33 @@ public class Library {
 		}
 		return false;
 	}
+	
+	/**
+	 * This method searches for a book using the ISBN
+	 * @param ISBN; the ISBN string we need to find
+	 * @return; returns the book if it finds a matching ISBN 
+	 * otherwise it returns null
+	 */
+	public Book searchByISBN(String ISBN) {
+		for(int i = 0; i < books.length; i++) {
+			if(books[i].getISBN().equals(ISBN)) {
+				return books[i];
+			}
+		}
+		return null; 
+	}
 
+	/**
+	 * This method run through the entire list of books and uses the toString 
+	 * generated in Book.java to print out the books
+	 */
+	public void displayBooks() {
+		for(int i = 0; i < books.length; i++) {
+			if(books[i] != null) {
+				System.out.println(books[i].toString());
+			}
+		}
+	}
 }	
 
 
